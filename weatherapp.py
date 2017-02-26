5d0a571369a3cb87db868068f105ea1ab4aac05d
Guan Wang <ggwangguan@gmail.com>
2017-02-25 19:09:35 -0800

    add form class

--- weatherapp.py
+++ weatherapp.py
@@ -5,19 +5,35 @@ import json
 import math
 from functools import partial
 
+class Form:
+    def __init__(self, temp_txt, mint_txt, maxt_txt, wther_txt, des_txt, wspd_txt,
+                 wdgr_txt, hmd_txt, slvl_txt, name_txt):
+        self.temp_txt = temp_txt
+        self.mint_txt = mint_txt
+        self.maxt_txt = maxt_txt
+        self.wther_txt = wther_txt
+        self.des_txt = des_txt
+        self.wspd_txt = wspd_txt
+        self.wdgr_txt = wdgr_txt
+        self.hmd_txt = hmd_txt
+        self.slvl_txt = slvl_txt
+        self.name_txt = name_txt
 
-def fill_form(temp_txt, mint_txt, maxt_txt, wther_txt, des_txt, wspd_txt, wdgr_txt, hmd_txt, slvl_txt, name_txt, city_value):
-    temp_txt.delete(1.0, tk.END)
-    mint_txt.delete(1.0, tk.END)
-    maxt_txt.delete(1.0, tk.END)
-    wther_txt.delete(1.0, tk.END)
-    des_txt.delete(1.0, tk.END)
-    wspd_txt.delete(1.0, tk.END)
-    wdgr_txt.delete(1.0, tk.END)
-    hmd_txt.delete(1.0, tk.END)
-    slvl_txt.delete(1.0, tk.END)
-    name_txt.delete(1.0, tk.END)
+def clear_form(form):
+    form.temp_txt.delete(1.0, tk.END)
+    form.mint_txt.delete(1.0, tk.END)
+    form.maxt_txt.delete(1.0, tk.END)
+    form.wther_txt.delete(1.0, tk.END)
+    form.des_txt.delete(1.0, tk.END)
+    form.wspd_txt.delete(1.0, tk.END)
+    form.wdgr_txt.delete(1.0, tk.END)
+    form.hmd_txt.delete(1.0, tk.END)
+    form.slvl_txt.delete(1.0, tk.END)
+    form.name_txt.delete(1.0, tk.END)
 
+
+def fill_form(form, city_value):
+    clear_form(form)
     citynm = city_value.get()
     params = dict(q=citynm, APPID = '26ee310aed6872889843892aa2ff1c1b')
     resp = requests.get('http://api.openweathermap.org/data/2.5/weather', params)
@@ -31,47 +47,47 @@ def fill_form(temp_txt, mint_txt, maxt_txt, wther_txt, des_txt, wspd_txt, wdgr_t
             temp_avg = main_section.get('temp')
             if temp_avg != None:
                 temp_avg = int(round(temp_avg - 273.15))
-                temp_txt.insert(tk.END, temp_avg)
-                temp_txt.insert(tk.END, '°C')
+                form.temp_txt.insert(tk.END, temp_avg)
+                form.temp_txt.insert(tk.END, '°C')
             else:
-                temp_txt.insert(tk.END, 'n/a')
+                form.temp_txt.insert(tk.END, 'n/a')
             # get min temperature
             temp_min = main_section.get('temp_min')
             if temp_min != None:
                 temp_min = int(math.floor(temp_min - 273.15))
-                mint_txt.insert(tk.END, temp_min)
-                mint_txt.insert(tk.END, '°C')
+                form.mint_txt.insert(tk.END, temp_min)
+                form.mint_txt.insert(tk.END, '°C')
             else:
-                mint_txt.insert(tk.END, 'n/a')
+                form.mint_txt.insert(tk.END, 'n/a')
             # get max temperature
             temp_max = main_section.get('temp_max')
             if temp_max != None:
                 temp_max = int(math.ceil(temp_max - 273.15))
-                maxt_txt.insert(tk.END, temp_max)
-                maxt_txt.insert(tk.END, '°C')
+                form.maxt_txt.insert(tk.END, temp_max)
+                form.maxt_txt.insert(tk.END, '°C')
             else:
-                maxt_txt.insert(tk.END, 'n/a')
+                form.maxt_txt.insert(tk.END, 'n/a')
             # get humidity info
             humidity = main_section.get('humidity')
             if humidity != None:
-                hmd_txt.insert(tk.END, humidity)
-                hmd_txt.insert(tk.END, '%')
+                form.hmd_txt.insert(tk.END, humidity)
+                form.hmd_txt.insert(tk.END, '%')
             else:
-                hmd_txt.insert(tk.END, 'n/a')
+                form.hmd_txt.insert(tk.END, 'n/a')
             # get sea level info
             sea_level = main_section.get('sea_level')
             if sea_level != None:
-                slvl_txt.insert(tk.END, sea_level)
-                slvl_txt.insert(tk.END,'m')
+                form.slvl_txt.insert(tk.END, sea_level)
+                form.slvl_txt.insert(tk.END,'m')
             else:
-                slvl_txt.insert(tk.END,'n/a')
+                form.slvl_txt.insert(tk.END,'n/a')
 
         else:
-            temp_txt.insert(tk.END, 'n/a')
-            mint_txt.insert(tk.END, 'n/a')
-            maxt_txt.insert(tk.END, 'n/a')
-            hmd_txt.insert(tk.END, 'n/a')
-            slvl_txt.insert(tk.END,'n/a')
+            form.temp_txt.insert(tk.END, 'n/a')
+            form.mint_txt.insert(tk.END, 'n/a')
+            form.maxt_txt.insert(tk.END, 'n/a')
+            form.hmd_txt.insert(tk.END, 'n/a')
+            form.slvl_txt.insert(tk.END,'n/a')
 
         # information from weather section
         weather_section = info.get('weather')
@@ -79,48 +95,48 @@ def fill_form(temp_txt, mint_txt, maxt_txt, wther_txt, des_txt, wspd_txt, wdgr_t
             # get the weather info
             short_description = weather_section[0].get('main')
             if short_description != None:
-                wther_txt.insert(tk.END, short_description)
+                form.wther_txt.insert(tk.END, short_description)
             else:
-                wther_txt.insert(tk.END, 'n/a')
+                form.wther_txt.insert(tk.END, 'n/a')
             # get the longer weather description
             long_description = weather_section[0].get('description')
             if long_description != None:
-                des_txt.insert(tk.END, long_description)
+                form.des_txt.insert(tk.END, long_description)
             else:
-                des_txt.insert(tk.END, 'n/a')
+                form.des_txt.insert(tk.END, 'n/a')
         else:
-            wther_txt.insert(tk.END, 'n/a')
-            des_txt.insert(tk.END, 'n/a')
+            form.wther_txt.insert(tk.END, 'n/a')
+            form.des_txt.insert(tk.END, 'n/a')
         # wind section
         wind_section = info.get('wind')
         if wind_section != None:
             # get the wind speed info
             wind_speed = wind_section.get('speed')
             if wind_speed != None:
-                wspd_txt.insert(tk.END, wind_speed)
-                wspd_txt.insert(tk.END, 'm/s')
+                form.wspd_txt.insert(tk.END, wind_speed)
+                form.wspd_txt.insert(tk.END, 'm/s')
             else:
-                wspd_txt.insert(tk.END, 'n/a')
+                form.wspd_txt.insert(tk.END, 'n/a')
             # get the wind degree info
             wind_degree = wind_section.get('deg')
             if wind_degree != None:
-                wdgr_txt.insert(tk.END, wind_degree)
-                wdgr_txt.insert(tk.END, '°')
+                form.wdgr_txt.insert(tk.END, wind_degree)
+                form.wdgr_txt.insert(tk.END, '°')
             else:
-                wdgr_txt.insert(tk.END, 'n/a')
+                form.wdgr_txt.insert(tk.END, 'n/a')
         else:
-            wspd_txt.insert(tk.END, 'n/a')
-            wdgr_txt.insert(tk.END, 'n/a')
+            form.wspd_txt.insert(tk.END, 'n/a')
+            form.wdgr_txt.insert(tk.END, 'n/a')
         # name section
         name_section = info.get('name')
         if name_section != None:
             # get name
-            name_txt.insert(tk.END, name_section)
+            form.name_txt.insert(tk.END, name_section)
         else:
-            name_txt.insert(tk.END, 'n/a')
+            form.name_txt.insert(tk.END, 'n/a')
 
     else:
-        temp_txt.insert(tk.END, '↑Invalid city name↑')
+        form.temp_txt.insert(tk.END, '↑Invalid city name↑')
 
 
 def main():
@@ -197,7 +213,8 @@ def main():
     slvl_txt = tk.Text(window, width=20, height=1)
     slvl_txt.grid(row=5, column=3)
 
-    command = partial(fill_form, temp_txt, mint_txt, maxt_txt, wther_txt, des_txt, wspd_txt, wdgr_txt, hmd_txt, slvl_txt, name_txt, city_value)
+    form = Form(temp_txt, mint_txt, maxt_txt, wther_txt, des_txt, wspd_txt, wdgr_txt, hmd_txt, slvl_txt, name_txt)
+    command = partial(fill_form, form, city_value)
 
     srch = tk.Button(window,  text='Search', command=command)
     srch.grid(row=0, column=2)
